28829a67a9db5a3be8cc152895e75c0f1859d2fe
RomainBoudet <romainoclock@gmail.com>
2022-09-27 16:50:51 +0200

    refacto done

--- source.js
+++ source.js
@@ -4,10 +4,33 @@ import { tarif } from "./tarif.js";
 import { agenda } from "./agenda.js";
 import { contact } from "./contact.js";
 
-const bigContainer = document.createElement("div");
-bigContainer.className = "bigContainer";
+const createHTML = (type, oneClasse, content, css, parent) => {
+  const elem = document.createElement(type);
 
-const divContainer = document.createElement("div");
+  if (content && !"") {
+    content = document.createTextNode(content);
+    elem.appendChild(content);
+  }
+
+  if (oneClasse && !"") {
+    oneClasse.map((item) => elem.classList.add(item));
+  }
+
+  if (css && !"") {
+    elem.style.cssText = css;
+  }
+
+  if(parent) {
+    parent.appendChild(elem);
+  }
+
+  return elem;
+};
+
+const bigContainer = createHTML("div");
+const divContainer = createHTML("div","","","dispay:flex;");
+
+bigContainer.className = "bigContainer";
 divContainer.className = "container";
 
 const obj = {
@@ -17,86 +40,58 @@ const obj = {
   contact: contact,
   propos: propos,
 };
-const arrayNav = ["acceuil", "tarif", "agenda", "contact", "propos"];
 
-const makeNav = (arrayNav) => {
-  const nav = document.createElement("nav");
-  nav.className = "nav";
-  nav.classList.add("justify-content-center", "font-weight-bold", "white");
-  nav.cssText = "padding:50px;";
+const makeNav = (obj) => {
+  const nav = createHTML(
+    "nav",
+    ["justify-content-center", "font-weight-bold", "white"],
+    "",
+    "padding:50px;color:grey"
+  );
 
-  for (const item of arrayNav) {
-    const button = document.createElement("button");
-    button.style.cssText = "margin:20px;border-radius:20px";
-    const a = document.createElement("a");
-    button.appendChild(a);
-    a.classList.add("nav-link");
+  for (const item in obj) {
+    const button = createHTML("button","","","margin:20px;border-radius:20px");
+    const a = createHTML("a",["nav-link"],`${item}`, "color:black;font-weight:bold;font-size:30px", button );
 
-    a.appendChild(document.createTextNode(`${item}`));
-
-    a.style.cssText = "color:black;font-weight:bold;font-size:30px";
-    nav.style.cssText = "color:grey";
     nav.appendChild(button);
     document.body.appendChild(nav);
 
     button.addEventListener("click", (event) => {
       event.preventDefault();
-
-      // on supprime ce qui il y a dans la div divContainer.
       divContainer.innerHTML = "";
-      console.log(obj[event.target.innerHTML]);
-      console.log(acceuil);
-      // on ajoute notre nouvelle div issue du tableau qui reprend l'import
-
       divContainer.appendChild(obj[event.target.innerHTML]);
     });
-  }
+  };
 };
-makeNav(arrayNav);
 
-divContainer.style.cssText = "dispay:flex;";
+makeNav(obj);
 
-const divContainer2 = document.createElement("div");
-divContainer2.style.cssText =
-  "display:flex;justify-content:space-evenly;padding:30px";
+const divContainer2 = createHTML("div", "", "", "display:flex;justify-content:space-evenly;padding:30px");
 
-const div1 = document.createElement("div");
-// un texte dans la 1ere div
-const p = document.createElement("p");
-const contentP = document.createTextNode("Bienvenue chez DiveDev !");
-p.appendChild(contentP);
-p.style.cssText = "text-align:center";
-p.style.cssText = "color:white;font-size:30px;padding:100px";
+const div1 = createHTML("div", "", "", "");
+const p = createHTML("p","","Bienvenue chez DiveDev !","color:white;font-size:30px;padding:100px;text-align:center");
 div1.appendChild(p);
 
-const div2 = document.createElement("div");
+const div2 = createHTML("div", "", "", "padding:20px");
 
-// une image dans la deuxiéme div
-const img1 = document.createElement("img");
+const img1 = createHTML("img", "", "", "");
 img1.src =
   "https://www.letelegramme.fr/images/2011/07/06/1362126_scubster-submarine-2.jpg";
-div2.cssText = "padding:20px";
 div2.appendChild(img1);
 
 divContainer2.appendChild(div1);
 divContainer2.appendChild(div2);
 
-const divContainer3 = document.createElement("div");
-divContainer3.style.cssText =
-  "display:flex;justify-content:space-evenly;padding:30px";
+const divContainer3 = createHTML("div", "", "","display:flex;justify-content:space-evenly;padding:30px");
 
-const div3 = document.createElement("div");
-const img2 = document.createElement("img");
+const div3 = createHTML("div","","","");
+const img2 = createHTML("img", "", "", "width:540px;height: auto;");
 img2.src =
   "https://dune-lalonde.com/wp-content/uploads/2015/03/plongee_autonome_1920-Guillaume-Ruoppolo-Dune-HD-104.jpg";
-img2.style.cssText = "width:540px;height: auto;";
 div3.appendChild(img2);
 
-const div4 = document.createElement("div");
-const p2 = document.createElement("p");
-const contentP2 = document.createTextNode("Consulter nos prestation !");
-p2.appendChild(contentP2);
-p2.style.cssText = "color:white;font-size:30px;padding:100px";
+const div4 = createHTML("div", "", "", "");
+const p2 = createHTML("p","", "Consulter nos prestation !", "color:white;font-size:30px;padding:100px");
 div4.appendChild(p2);
 
 divContainer3.appendChild(div3);
