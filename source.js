2f36a7af63b9b0008278862dc7fe8e28f1354249
RomainBoudet <romainoclock@gmail.com>
2022-09-27 17:02:44 +0200

    refact V2

--- source.js
+++ source.js
@@ -27,8 +27,8 @@ const createHTML = (type, oneClasse, content, css, parent) => {
   return elem;
 };
 
-const bigContainer = createHTML("div");
-const divContainer = createHTML("div","","","dispay:flex;");
+const bigContainer = createHTML("div", "", "", "");
+const divContainer = createHTML("div","","","dispay:flex;", bigContainer);
 
 bigContainer.className = "bigContainer";
 divContainer.className = "container";
@@ -66,39 +66,27 @@ const makeNav = (obj) => {
 
 makeNav(obj);
 
-const divContainer2 = createHTML("div", "", "", "display:flex;justify-content:space-evenly;padding:30px");
+const divContainer2 = createHTML("div", "", "", "display:flex;justify-content:space-evenly;padding:30px", divContainer);
 
-const div1 = createHTML("div", "", "", "");
+const div1 = createHTML("div", "", "", "", divContainer2);
 const p = createHTML("p","","Bienvenue chez DiveDev !","color:white;font-size:30px;padding:100px;text-align:center");
 div1.appendChild(p);
 
-const div2 = createHTML("div", "", "", "padding:20px");
+const div2 = createHTML("div", "", "", "padding:20px", divContainer2);
 
-const img1 = createHTML("img", "", "", "");
+const img1 = createHTML("img", "", "", "", div2);
 img1.src =
   "https://www.letelegramme.fr/images/2011/07/06/1362126_scubster-submarine-2.jpg";
-div2.appendChild(img1);
 
-divContainer2.appendChild(div1);
-divContainer2.appendChild(div2);
+const divContainer3 = createHTML("div", "", "","display:flex;justify-content:space-evenly;padding:30px", divContainer);
 
-const divContainer3 = createHTML("div", "", "","display:flex;justify-content:space-evenly;padding:30px");
-
-const div3 = createHTML("div","","","");
-const img2 = createHTML("img", "", "", "width:540px;height: auto;");
+const div3 = createHTML("div","","","", divContainer3);
+const img2 = createHTML("img", "", "", "width:540px;height: auto;", div3);
 img2.src =
   "https://dune-lalonde.com/wp-content/uploads/2015/03/plongee_autonome_1920-Guillaume-Ruoppolo-Dune-HD-104.jpg";
-div3.appendChild(img2);
 
-const div4 = createHTML("div", "", "", "");
-const p2 = createHTML("p","", "Consulter nos prestation !", "color:white;font-size:30px;padding:100px");
-div4.appendChild(p2);
 
-divContainer3.appendChild(div3);
-divContainer3.appendChild(div4);
+const div4 = createHTML("div", "", "", "", divContainer3);
+const p2 = createHTML("p","", "Consulter nos prestation !", "color:white;font-size:30px;padding:100px", div4);
 
-divContainer.appendChild(divContainer2);
-divContainer.appendChild(divContainer3);
-
-bigContainer.appendChild(divContainer);
 document.body.appendChild(bigContainer);
