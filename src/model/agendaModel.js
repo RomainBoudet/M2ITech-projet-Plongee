6904bb9c433a0a60d5fbb165ae515c4dc4d6d52a
RomainBoudet <romainoclock@gmail.com>
2022-09-30 11:03:30 +0200

    refacto data

--- src/model/agendaModel.js
+++ src/model/agendaModel.js
@@ -1,28 +1,5 @@
 
-const agendaData = {
-  src: "https://media.istockphoto.com/photos/megalodon-scene-3d-illustration-picture-id1004792742?k=20&m=1004792742&s=612x612&w=0&h=iF4-rRjST6WLuEpCmVpqh_jEFyZw6k082gT2ZMavt5Q=",
-  title: "Bienvenue sur la page Agenda !",
-  subTitle: "Vous retrouverez sur cette page un agenda invisible...",
-};
-
-const tarifData = {
-    src:   "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Mola_tecta.jpg/290px-Mola_tecta.jpg",
-    title:   "Bienvenue sur la page Tarif !",
-    subTitle: "Vous allez prendre cher ! 😱",
-};
-
-const proposData = {
-    src:    "https://media.istockphoto.com/photos/underwater-cute-salt-water-porcupine-balloonfish-fish-smiling-picture-id638309968?k=20&m=638309968&s=612x612&w=0&h=1RjK5b4ZJFupk4e-KKkl6FaJqpZB1IbbDAkM4QioenE=",
-    title:   "Bienvenue sur la page A propos !",
-    subTitle:  "Vous retrouverez sur cette page des infos incroyable",
-};
-
-const contactData = {
-    src:     "https://www.conservation-nature.fr/wp-content/uploads/2021/02/la-baleine-bleue.jpg",
-    title:    "Rhô... la belle page !",
-    subTitle:  "Equipe de dev fantome, pour des réclamations renseignez vous auprés des commerciaux... ! 😇",
-};
-
+import { agendaData,tarifData, proposData, contactData } from "../../data/cardData.js";
 
 export class Card {
   src;
